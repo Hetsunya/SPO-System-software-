8ec6e5ce4695ed884ee1d3241e604ee1872100fd
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2023-02-26 13:52:32 +0500

    Add files via upload

--- lab1/task.sh
+++ lab1/task.sh
@@ -1,27 +1,18 @@
-#!/bin/bash
 echo "--------------------------Переменные оболочки--------------------------"
 for n in $@;
 	do
-		stat $n
-done
-#basename $n
-		#date -r $n;
+    stat -c '%n' $n
+		stat -c '%x' $n
+		stat -c '%y' $n
+		stat -c '%w' $n
+  done
 echo "--------------------------gawk--------------------------"
-gawk -F stat $1  
-
-gawk 'BEGIN  {
-    format = PROCINFO["strftime"]
-    exitval = 0
-
-    if (ARGC > 2)
-        exitval = 1
-    else if (ARGC == 2) {
-        format = ARGV[1]
-        if (format ~ /^\+/)
-            format = substr(format, 2)   # remove leading +
-    }
-    print strftime(format)
-    exit exitval
-}' "$@"
-
+echo $(stat -c '%y' $@) $(stat -c '%w' $@) $(stat -c '%x' $@) | gawk '{print "File last modified time:",$1,$2,"\n","File creation time:",$4,$5,"\n","File last access time:",$7,$6}'
 echo "--------------------------Командная подстановка--------------------------"
+for n in $@;
+  do
+    script_name=`stat -c '%n' $n`
+    script_time=`stat -c '%x' $n; stat -c '%y' $n; stat -c '%w' $n;`
+    echo "$script_name 
+$script_time"
+  done
